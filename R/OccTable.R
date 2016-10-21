c606e6163ca5dbedc277a5b6799735a5199a8f09
guangchun <hanguangchun@gmail.com>
2016-10-20 17:07:00 -0800

    list2OccTable1 is more speedy, need further tests

--- R/OccTable.R
+++ R/OccTable.R
@@ -24,3 +24,28 @@ list2OccTable <- function(lst) {
     res
 }
 
+#' convert a named list to an Ocurrence table(emerged 1, absent 0), this one is speedy, need test...
+#'
+#' this function convert a names list  with length n to a matrix with dim x*n
+#' x is the total number of unique elements stored in the list
+#' @param lst a deg list
+#' @return a matrix with 0s and 1s
+#' @export
+list2OccTable1 <- function(lst) {
+    if (is.null(attributes(lst)$names))
+        stop("input list should have names")
+    if (length(lst) <= 1)
+        stop("length of input list <= 1")
+    elems <- unique(unlist(lst))
+    if (length(elems) <= 1)
+        stop("number of uniqued elements in the input list <= 1")
+    res <- lapply(lst, function(x, elems) {
+		   tmp <- rep(0,length=length(elems))
+		   tmp[ elems %in% x ] <- 1
+		   tmp
+	      },elems=elems)
+    res1 <- as.matrix(as.data.frame(res))
+    rownames(res1) <- elems
+    colnames(res1) <- names(lst)
+    res1
+}
