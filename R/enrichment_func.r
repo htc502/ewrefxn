60c4a9facd371442d51bb1f864ccb339430cdca2
Han <ghan1@Guangchun.local>
2018-03-06 13:54:12 -0600

    fix searchNCBI wont load and add enrichment_func

--- R/enrichment_func.r
+++ R/enrichment_func.r
@@ -5,7 +5,8 @@
 #' @param PathGenes a vector of genes belonging to a pathway
 #' @param interestGenes a vector of genes of interest
 #' @param totalGenes a vector of background genes
-#'
+#' @param comet_exact_test logical for comet exact test of mutual exclusive(requires package cometExactTest)
+#' @param mutmatplot plot mutation mat for cometExactTest result
 #' @return a list
 #' @author guangchun
 #' @keywords enrich.test
@@ -15,7 +16,7 @@
 #' @export
 
 
-enrich.test = function(PathGenes, interestGenes, totalGenes) {
+enrich.test = function(PathGenes, interestGenes, totalGenes,comet_exact_test=F,mutmatplot=T) {
 ################################################
 ###           | inPath | not inPath |
 ###-------------------------------------------
@@ -25,6 +26,9 @@ enrich.test = function(PathGenes, interestGenes, totalGenes) {
 ###           |  nPath |            | nAll
 ################################################
   ##remove genes not included in reference gene set
+  if(comet_exact_test) {
+    if(!ewrefxn::require0('cometExactTest')) stop('error installing/loading cometExactTest,stop')
+  }
   PathGenes = intersect(PathGenes,totalGenes)
   interestGenes = intersect(interestGenes, totalGenes)
   
@@ -43,10 +47,17 @@ enrich.test = function(PathGenes, interestGenes, totalGenes) {
   p1 <- phyper(a-1,nPath, nAll-nPath,nPredict,lower.tail=F)
   p2 <-1 - phyper(a-1,nPath, nAll-nPath,nPredict)
   tmp <- c(a,b,c,d,p,p1,p2)
+
+  
   res = list(mat=m,p=list('fisher.exact'=p,
                           'phyper1'=p1,
                           'phyper2'=p2),
             overlapped=intersect(PathGenes, interestGenes))
+  ##mutual exclusive test
+  if(comet_exact_test) {
+  p.comet = comet_exact_test(c(a,b,c,d),mutmatplot = mutmatplot)
+  res$cometTest = p.comet
+  }
   res
 }
 
--- R/searchNCBI.R
+++ R/searchNCBI.R
@@ -56,4 +56,3 @@ searchNCBI = function(keyword, db = 'pubmed',out = NULL) {
   write_tsv(outputTbl, out)
   outputTbl
 }
-
