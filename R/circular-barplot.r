519f01619905a2bee0df551f60f45e1fd5c3af88
ewre <ewre@ewredeiMac.local>
2019-05-04 15:48:08 -0500

    add tbl2hmap

---
+++ R/circular-barplot.r
@@ -0,0 +1,10 @@
+## Creation date: 2018-04-21 21:09:04 
+
+## What does it do: circular stacked barplot
+
+## Data we need (Only include data you really need, don't put everything all together. 
+## You can't combine them before you can correctly divide to get them.): 
+
+## Result plot 1
+
+## End of the script
